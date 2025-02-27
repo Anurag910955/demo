6a76ce4ad3ec0748ab9ef9d36bbbf48e90b95dc4
Anurag910955 <145783931+Anurag910955@users.noreply.github.com>
2025-02-27 15:55:09 +0530

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/resume_builder", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB");
+    await mongoose.connect("mongodb+srv://anuragsen:anuragsen@cluster0.jpdbz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("Connected to MongoDB Atlas");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1); // Exit process if connection fails
   }
 };
 
-module.exports = connectDB; // Export as a function
+module.exports = connectDB;
